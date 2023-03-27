e003d9b64cfad89d477c05370c4178354f5b8d79
Jro Marcon <jromarcon18@gmail.com>
2023-03-26 22:50:22 -0700

    First commit

---
+++ BeReal/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  BeReal
+//
+//  Created by Jro Marcon on 3/21/23.
+//
+
+import UIKit
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //Initialize Parse SDK
+        ParseSwift.initialize(applicationId: "MsnelG0k1JHR0tN6VpER2zZmbzoQNRyDG75AdiUI",
+                              clientKey: "Lmzb1QbyxGt0ILAgpiXh3MTJ4ZN6Ze0IAzJCNRHX",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
+    
+        
+       
+        
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+}
+
+
+
+
+
+
